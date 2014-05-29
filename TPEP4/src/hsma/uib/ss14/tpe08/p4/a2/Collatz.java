9d3c5d3f5c4b1fef4d65fb914f2f7e71d2a4e7d8
mustang2015 <giangpham92@gmail.com>
2014-05-29 10:22:19 +0200

    2

--- TPEP4/src/hsma/uib/ss14/tpe08/p4/a2/Collatz.java
+++ TPEP4/src/hsma/uib/ss14/tpe08/p4/a2/Collatz.java
@@ -1,14 +1,29 @@
 package hsma.uib.ss14.tpe08.p4.a2;
+
 /**
  * 
  * @author Giang Pham
  * @author Joshua Barsoum
  * @author Hunar Mawlod
  * 
- *
+ * 
  */
 public class Collatz {
-	private class Iterator{
+	
+	//Programm aus dem Internet. Lösung ist ohne iterator!
+
+	public static void collatz(int n) {
+		System.out.print(n + " ");
+		if (n == 1) {
+			return;
+		} else if (n % 2 == 0) {
+			collatz(n / 2);
+		} else {
+			collatz(3 * n + 1);
+		}
+	}
+
+	private class Iterator {
 		
 	}
 
--- TPEP4/src/hsma/uib/ss14/tpe08/p4/a2/Testklasse.java
+++ TPEP4/src/hsma/uib/ss14/tpe08/p4/a2/Testklasse.java
@@ -1,15 +1,19 @@
 package hsma.uib.ss14.tpe08.p4.a2;
-import static org.junit.Assert.*;
-import org.junit.Test;
 
+/*
+ import static org.junit.Assert.*;
+ import org.junit.Test;
+ */
 /**
  * 
  * @author Giang Pham
  * @author Joshua Barsoum
  * @author Hunar Mawlod
  * 
- *
+ * 
  */
 public class Testklasse {
-
+	public static void main(String[] args) {
+		Collatz.collatz(13);
+	}
 }
